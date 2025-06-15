254182bed528f084d83a82409ad0c477a146bcbf
Riyazshaik756 <cse240001068@iiti.ac.in>
2025-06-15 11:42:15 +0530

    Notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,3 +1,25 @@
+import './Notification.css';
+import image from './image.png';
+
+const notifi =[{anouncment_photo:image,anouncement_heading:'Headind 1',anouncment_info:'This is the first announcement.'},
+  {anouncment_photo:image,anouncement_heading:'Heading 2',anouncment_info:'This is the first announcement.'},
+  {anouncment_photo:image,anouncement_heading:'Heading 3',anouncment_info:'This is the first announcement.'}];
 export default function Notification() {
-  return <div>Notification content</div>;
+  return( 
+  <div>
+    <div id="con">
+     { notifi.map((e,index)=>(
+      <div key={index} className="notifi">
+        <img src={e.anouncment_photo} alt="image" />
+        <div className='notifi_info'>
+        <h1>{e.anouncement_heading}</h1>
+        <p>{e.anouncment_info}</p>
+        </div>
+      </div>
+
+      ))}
+
+    </div>
+  </div>
+);
 }
