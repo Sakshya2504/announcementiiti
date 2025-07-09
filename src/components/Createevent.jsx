59cb0c3c7777087dbdbc8de827331ffe5a7993f6
Sakshya2504 <singhkaserasakshya2504@gamil.com>
2025-07-09 08:10:48 +0530

    Sakshya-23

--- src/components/Createevent.jsx
+++ src/components/Createevent.jsx
@@ -1,157 +1,83 @@
-import { React, useState } from "react";
+// src/components/ClubPage.jsx
+// import image from './Images/iiti.png'
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-function Createevent() {
-  const navigate = useNavigate();
-  // This component allows users to create an event for a club
-  // It includes a form where users can input the event name, date and time, conducted by, and event info
-  // useState is used to manage the state of the event information
-  const [logininfo, setlogininfo] = useState({
-    EventName: "",
-    EventDateAndTime: "",
-    ConductedBy: "",
-    EventInfo: "",
-  });
+function ClubPage() {
+  const [clubs, setClubs] = useState([]);
 
-  // This function updates the state of logininfo when the user types in the input fields
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setlogininfo((prev) => ({ ...prev, [name]: value }));
-  };
 
-  const handleVerification = async (email) => {
-    try {
-      const res = await fetch("http://localhost:3000/api/verifyadmin", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email }),
-      });
 
-      const data = await res.json();
-      return data.authorized;
-    } catch (err) {
-      console.error("Verification error:", err);
-      alert("Something went wrong. Try again later.");
-      return false;
-    }
-  };
+  useEffect(() => {
+    const fetchclubs = async () => {
+      // Fetch API is used to send a GET request to the server with the clubs data
+      // The server will then process this data and return the list of clubs
+      try {
+        const res = await fetch("/api/clubs");
+        const data = await res.json();
+        console.log(data);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+        const updatedclubs = data.map((cl, index) => ({
+          // ...clu is used to spread the properties of the event object
+          ...cl,
+          name: cl.name || index + 1,
+          image: Image,
+        }));
 
-    const email = prompt("Enter your email to verify admin access:");
-    if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
-      alert("Invalid or missing email.");
-      return;
-    }
-
-    const authorized = await handleVerification(email);
-    if (!authorized) {
-      alert("You're not authorized to create events.");
-      navigate("/individualclubpage");
-      return;
-    }
-
-    try {
-      const res = await fetch("http://localhost:3000/Createevent", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(logininfo),
-      });
-
-      const result = await res.json();
-
-      if (res.ok) {
-        alert(result.message || "Event creation successful");
-        setlogininfo({
-          EventName: "",
-          EventDateAndTime: "",
-          ConductedBy: "",
-          EventInfo: "",
-        });
-        navigate("/");
-      } else {
-        alert(result.message || "Event creation failed");
+        setClubs(updatedclubs);
+      } catch (err) {
+        console.error("Failed to load clubs:", err);
       }
-    } catch (error) {
-      console.error("Error submitting event:", error);
-      alert("Event submission failed");
-    }
-  };
+    };
+    //fecthclubs is called to fetch the clubs when the component mounts
+    // This will trigger the useEffect hook and fetch the clubs from the server
+    fetchclubs();
+  }, []);
 
   return (
-    <>
-      <div className="createvent w-full h-full flex justify-center items-center bg-[rbga(1,1,27)]">
-        <div className="createevent_con relative flex flex-col w-[90%] md:w-[400px] m-[30px] p-[20px] bg-[linear-gradient(to_right,_rgba(6,182,212,0.3),_rgba(59,130,246,0.3))]  border-2 rounded-[10px] border-black  shadow-[0px_4px_15px_rgba(0, 0, 0, 0.1)] hover:shadow-[0_0_25px_#00ffff66]">
-          <button
-            className="back absolute top-[2px] right-[2px] cursor-pointer w-[30px] h-[30px] rounded-[5px] hover:bg-red-500 "
-            onClick={() => navigate(-1)}
-          >
-            {" "}
-            ❌{" "}
-          </button>
-
-          <div className="createvent bg-white text-black dark:bg-[#01011b] dark:text-white p-4 rounded-md shadow-md">
-            <div className="createevent_con">
-              <button className="back" onClick={() => navigate(-1)}>
-                {" "}
-                ❌{" "}
-              </button>
-
-              <form
-                action="/individualclubpage"
-                onSubmit={handleSubmit}
-                className="flex flex-col items-center justify-center  w-[100%] h-[100%]"
-              >
-                <h2 className="text-white font-bold text-[22px] ">
-                  Event Detailes
-                </h2>
-                <input
-                  type="text"
-                  placeholder="Event Name"
-                  className="text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]"
-                  name="EventName"
-                  value={logininfo.EventName}
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  placeholder="Event Date And Time"
-                  className="text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]"
-                  name="EventDateAndTime"
-                  value={logininfo.EventDateAndTime}
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  placeholder="Conducted By"
-                  name="ConductedBy"
-                  className="text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]"
-                  value={logininfo.ConductedBy}
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  placeholder="Event Info"
-                  name="EventInfo"
-                  className="text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]"
-                  value={logininfo.EventInfo}
-                  onChange={handleChange}
-                />
-                <button
-                  type="submit"
-                  className="submitbutton block w-[90%] md:w-[200px] m-[20px] p-[12px] text-white text-[18px] font-bold bg-[linear-gradient(to_right,_#007bff,_#00c3ff)] border-none rounded-[8px] cursor-pointer hover:bg-[linear-gradient(to_right,_#0056b3,_#0097d1)] hover:scale-105 transition-[background,transform] duration-[300ms,200ms]"
-                >
-                  Submit
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div
+      id="council"
+      className="flex mt-0 xs:mt-30 bg-white dark:bg-[#01011b] transition-colors duration-300 flex-wrap place-content-center gap-0.5"
+    >
+      {clubs.map((club) => (
+        <ClubCard key={club.name} {...club} />
+      ))}
+    </div>
   );
 }
 
-export default Createevent;
+function ClubCard({ name }) {
+  return (
+    <div className=" clubpage bg-gradient-to-r from-cyan-500/5 to-blue-500/5 border-2 border-[#87CEEB]  pt-20 flex flex-col items-center pb-10 sm:w-[25vh] sm:h-[40vw] md:w-[35vh] md:h-[40vw] lg:w-[50vh] lg:h-[30vw] ">
+      <div className="bg-gradient-to-r from-cyan-200/10 to-blue-200/10 dark:from-cyan-800/10 dark:to-blue-800/10 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md transition-colors duration-300 pt-10 px-4 flex flex-col items-center justify-between sm:w-[25vh] sm:h-[40vw] md:w-[35vh] md:h-[40vw] lg:w-[50vh] lg:h-[30vw]">
+        <div className="img-cont lg:w-52 lg:h-52 md:w-40 md:h-40 sm:w-30 sm:h-40 flex items-center justify-center lg:mb-5 md:m-2 xs:w-40 xs:h-40 cursor-pointer hover:shadow-xl xxs:h-40">
+          <img
+            src={club.image}
+            alt={name}
+            width={200}
+            height={200}
+            className="lg:w-full lg:h-full sm:w-4/5 sm:h-4/5 md:w-[90%] md:h-[90%] xxs:w-[70%] xxs:h-[70%] md:mt-5 sm:mt-3 xxs:mt-10 border-sky-600 "
+          />
+        </div>
+        <div className="heading-cont flex items-center justify-center lg:w-60 lg:h-12 sm:w-40 sm:h-8 md:w-50 md:h-10 xxs:w-40 xxs:h-10">
+          <h5 className="mb-1 text-center text-black dark:text-white lg:text-xl xxs:text-sm md:text-md lg:font-medium">
+            {name}
+          </h5>
+        </div>
+        <div className="w-52 h-12 mb-10 mt-1 flex items-center justify-center cursor-pointer xl:border-b-4 border-blue-500">
+          <Link
+            to={`/clubs/${encodeURIComponent(name)}`}
+            className="px-4 py-2 max-sm:mb-8 xxs:text-xs sm:text-sm font-medium text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 rounded-lg"
+          >
+            See more
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ClubPage;
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,51 +1,31 @@
 import exampleImage from './Images/image.png'; // relative to Events.jsx
-import { useEffect, useState , React } from 'react';
+import { useEffect, useState, React } from 'react';
 import './Animation.css'
 import { useNavigate } from 'react-router-dom';
-
-// const events = [
-//   {
-//     id: 1,
-//     time: '6pm today',
-//     location: 'Near tea post',
-//     name: 'Nukkad Natak',
-//     club: 'Avana and Aaina collab',
-//     image: exampleImage,
-//   },
-//   ...Array.from({ length: 8 }, (_, i) => ({
-//     id: i + 2,
-//     time: '6pm today',
-//     location: 'Near tea post',
-//     name: 'Nukkad Natak',
-//     club: 'Avana and Aaina collab',
-//     image: exampleImage,
-//   })),
-// ];
-
 
 export default function Events(props) {
   // This component fetches and displays a list of events
   // It uses the useState hook to manage the state of events
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const [selectedEventId, setSelectedEventId] = useState(null);
   const [events, setEvents] = useState([]);
   const [registrationCounts, setRegistrationCounts] = useState({});
-  const [register,setregister]=useState(false);
+  const [register, setregister] = useState(false);
   const [filteredEvents, setFilteredEvents] = useState([]);
-    const [registerinfo, setregisterinfo] = useState({
-        Name: "",
-        EmailAddress: "",
-        RollNumber:"",
-        Program:"",
-        Branch:"",
-        PhoneNumber: "",
-       
-      })
-      const handleChange = (e) => {
-      const { name, value } = e.target;
-      setregisterinfo(prev => ({ ...prev, [name]: value }));
-    };
-     
+  const [registerinfo, setregisterinfo] = useState({
+    Name: "",
+    EmailAddress: "",
+    RollNumber: "",
+    Program: "",
+    Branch: "",
+    PhoneNumber: "",
+
+  })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setregisterinfo(prev => ({ ...prev, [name]: value }));
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -55,9 +35,9 @@ export default function Events(props) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(registerinfo),
       });
-      if(res.ok){
+      if (res.ok) {
         alert('Registered successfully!');
-        setregister(false); 
+        setregister(false);
         setregisterinfo({
           Name: '',
           EmailAddress: '',
@@ -68,16 +48,15 @@ export default function Events(props) {
         });
         navigate('/');
       }
-      
-      
+
+
     } catch (err) {
       console.error(err);
       alert('Something went wrong. Please try again.');
     }
-    };
+  };
 
-  
-  
+
   // useEffect is used to fetch the events from the server when the component mounts
   // It sends a GET request to the server to retrieve the events data
   useEffect(() => {
@@ -127,97 +106,97 @@ export default function Events(props) {
 
   return (
     <>
-    
-    <div className="p-4 bg-[#01011b] min-h-screen">
-      <h1 className="text-3xl font-bold mb-6 text-white text-center">Events</h1>
 
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="p-4 bg-[#01011b] min-h-screen">
+        <h1 className="text-3xl font-bold mb-6 text-white text-center">Events</h1>
+
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {(filteredEvents.length > 0 ? filteredEvents : events).map((event) => (
-          <div
-            key={event.id}
-            className="event-detail rounded-2xl shadow-md p-4  bg-gradient-to-r from-cyan-500/5 to-blue-500/5 space-y-3 border-2 border-[#87CEEB]
+            <div
+              key={event.id}
+              className="event-detail rounded-2xl shadow-md p-4  bg-gradient-to-r from-cyan-500/5 to-blue-500/5 space-y-3 border-2 border-[#87CEEB]
             hover:border-[#33bbcf] hover:scale-[1.03] "
-          >
-            
+            >
+
               {filteredEvents.length === 0 && props.searchQuery && (
                 <p className="text-white text-center mt-4">
                   No events found for "{props.searchQuery}"
                 </p>
               )}
 
-            
-    <div className='box block transform-3d perspective-[1000px] hover:rotate-y-180 transition delay-[0.3s]'>
-       <div className='card grid relative transform-3d'>
-          <div id='front ' className="event-description  col-start-1 row-start-1 space-y-1 relative backface-hidden">
-              <div className="event-logo flex items-center justify-center">
-              <img
-                alt="Event Logo"
-                src={event.image}
-                className="h-100 w-100"
-              />
+
+              <div className='box block transform-3d perspective-[1000px] hover:rotate-y-180 transition delay-[0.3s]'>
+                <div className='card grid relative transform-3d'>
+                  <div id='front ' className="event-description  col-start-1 row-start-1 space-y-1 relative backface-hidden">
+                    <div className="event-logo flex items-center justify-center">
+                      <img
+                        alt="Event Logo"
+                        src={event.image}
+                        className="h-100 w-100"
+                      />
+                    </div>
+                    <p className="text-white font-medium">🕒 Time: {event.EventDateAndTime}</p>
+                    <p className="text-white font-medium">📍 Info: {event.EventInfo}</p>
+                    <p className="text-white font-semibold">🎭 Event: {event.EventName}</p>
+                    <p className="text-white font-semibold">
+                      📋 Registered: {registrationCounts[event._id] ?? '...'} students
+                    </p>
+                    <p className="text-white font-semibold">
+                      🧑‍💼 Conducted by: {event.ConductedBy}
+                    </p>
+
+
+                  </div>
+                  <div id='back' className=' absolute col-start-1 row-start-1 flex flex-col justify-center items-center top-0 left-0 w-[100%] h-[100%] backface-hidden rotate-y-180 '>
+                    <h1 className='text-[#11E3FB] font-bold text-[32px] pt-[10px] pb-[10px]'>{event.EventName}</h1>
+                    <p className='text-white font-bold'> {event.EventInfo}</p>
+                    <button
+                      className="mt-10 bg-blue-500 cursor-pointer text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+                      id={`joinEvent${event.id}`} onClick={() => {
+                        if (props.issignup) {
+                          setSelectedEventId(event.id);
+                          setregister(true);
+                        } else {
+                          navigate('/signup');
+                          alert('Please verify your email to continue.');
+                        }
+                      }}
+                    >
+                      Join Event
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
-              <p className="text-white font-medium">🕒 Time: {event.EventDateAndTime}</p>
-              <p className="text-white font-medium">📍 Info: {event.EventInfo}</p>
-              <p className="text-white font-semibold">🎭 Event: {event.EventName}</p>
-                  <p className="text-white font-semibold">
-                    📋 Registered: {registrationCounts[event._id] ?? '...'} students
-                  </p>
-                  <p className="text-white font-semibold">
-                    🧑‍💼 Conducted by: {event.ConductedBy}
-                  </p>
+
+          ))}
+        </div>
+
+      </div>
+      {register &&
+
+        <div className='fixed top-0 z-1000 w-[100%] h-[100%] flex justify-center items-center '>
+          <div className=' fixed flex flex-col w-[90%] md:w-[400px] m-[30px] p-[20px] bg-[linear-gradient(to_right,_rgba(6,182,212),_rgba(59,130,246))]  border-2 rounded-[10px] border-black  shadow-[0px_4px_15px_rgba(0, 0, 0, 0.1)]  hover:shadow-[0_0_25px_#00ffff66]'>
+            <button className='back absolute top-[2px] right-[2px] cursor-pointer w-[30px] h-[30px] rounded-[5px] hover:bg-red-500 ' onClick={() => setregister(false)}> ❌ </button>
+
+            <form action="/" onSubmit={handleSubmit} className='flex flex-col items-center justify-center  w-[100%] h-[100%]'>
+
+
+              <h2 className='text-white font-bold text-[22px] '>Event Registration</h2>
+              <input type="text" placeholder=' Name' className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='Name' value={registerinfo.Name} onChange={handleChange} />
+              <input type="text" placeholder='EmailAddress' className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='EmailAddress' value={registerinfo.EmailAddress} onChange={handleChange} />
+              <input type="text" placeholder='RollNumber' className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='RollNumber' value={registerinfo.RollNumber} onChange={handleChange} />
+              <input type="text" placeholder='Program' className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='Program' value={registerinfo.Program} onChange={handleChange} />
+              <input type="text" placeholder='Branch' className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='Branch' value={registerinfo.Branch} onChange={handleChange} />
+              <input type="text" placeholder='PhoneNumber' name='PhoneNumber' className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' value={registerinfo.PhoneNumber} onChange={handleChange} />
+              <button type="submit" className="submitbutton block w-[90%] md:w-[200px] m-[20px] p-[12px] text-white text-[18px] font-bold bg-[linear-gradient(to_right,_#007bff,_#00c3ff)] border-none rounded-[8px] cursor-pointer hover:bg-[linear-gradient(to_right,_#0056b3,_#0097d1)] hover:scale-105 transition-[background,transform] duration-[300ms,200ms]">Register</button>
+
+            </form>
 
 
           </div>
-          <div id='back' className=' absolute col-start-1 row-start-1 flex flex-col justify-center items-center top-0 left-0 w-[100%] h-[100%] backface-hidden rotate-y-180 '>
-                <h1 className='text-[#11E3FB] font-bold text-[32px] pt-[10px] pb-[10px]'>{event.EventName}</h1>
-                <p className='text-white font-bold'> {event.EventInfo}</p>
-                 <button
-                className="mt-10 bg-blue-500 cursor-pointer text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-                    id={`joinEvent${event.id}`} onClick={() => {
-                      if (props.issignup) {
-                        setSelectedEventId(event.id);
-                        setregister(true);
-                      } else {
-                        navigate('/signup');
-                        alert('Please verify your email to continue.');
-                      }
-                }}
-              >
-                Join Event
-              </button>
-          </div>
-      </div>
-    </div>
-</div>
-          
-        ))}
-      </div>
+        </div>
 
-    </div>
-    {register&&
-      
-   <div className='fixed top-0 z-1000 w-[100%] h-[100%] flex justify-center items-center '>
-      <div className=' fixed flex flex-col w-[90%] md:w-[400px] m-[30px] p-[20px] bg-[linear-gradient(to_right,_rgba(6,182,212),_rgba(59,130,246))]  border-2 rounded-[10px] border-black  shadow-[0px_4px_15px_rgba(0, 0, 0, 0.1)]  hover:shadow-[0_0_25px_#00ffff66]'>
-        <button className='back absolute top-[2px] right-[2px] cursor-pointer w-[30px] h-[30px] rounded-[5px] hover:bg-red-500 ' onClick={()=>setregister(false)}> ❌ </button>
-        
-          <form action="/" onSubmit={handleSubmit} className='flex flex-col items-center justify-center  w-[100%] h-[100%]'>
-     
-    
-      <h2 className='text-white font-bold text-[22px] '>Event Registration</h2>
-      <input type="text" placeholder=' Name' className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='Name' value={registerinfo.Name} onChange={handleChange} />
-      <input type="text" placeholder='EmailAddress'className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='EmailAddress' value={registerinfo.EmailAddress} onChange={handleChange}/>
-      <input type="text" placeholder='RollNumber'className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='RollNumber' value={registerinfo.RollNumber} onChange={handleChange}/>
-      <input type="text" placeholder='Program'className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='Program' value={registerinfo.Program} onChange={handleChange}/>
-      <input type="text" placeholder='Branch'className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' name='Branch' value={registerinfo.Branch} onChange={handleChange}/>
-      <input type="text" placeholder='PhoneNumber' name='PhoneNumber'className='text-black block bg-white border rounded-[10px] w-[90%] md:w-[75%] h-[50px] m-[10px]' value={registerinfo.PhoneNumber} onChange={handleChange}/>
-      <button type="submit" className="submitbutton block w-[90%] md:w-[200px] m-[20px] p-[12px] text-white text-[18px] font-bold bg-[linear-gradient(to_right,_#007bff,_#00c3ff)] border-none rounded-[8px] cursor-pointer hover:bg-[linear-gradient(to_right,_#0056b3,_#0097d1)] hover:scale-105 transition-[background,transform] duration-[300ms,200ms]">Register</button>
-    
-      </form>
-    
-
-     </div>
-   </div>
-
-    }
-  </>);
+      }
+    </>);
 }
